8edcf9ef970745eb6b4a073f4a1e2b3592c824e3
nidhinsankar <nidhinsankar1@gmail.com>
2024-03-14 22:54:49 +0530

    implemented CRUD api for snapstock

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 import models
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/products',methods=['GET'])
 def products():  # GET ALL PRODUCTS
     return jsonify(models.products)
 
+@app.route('/addproduct',methods=['POST'])
+def addProduct():
+    jsonData = request.json
+    # models.products.append(jsonData)
+    return jsonify({"result":"added successfully","data":jsonData})
+
+@app.route('/deleteproduct/<int:productid>',methods=['DELETE'])
+def deleteProduct(productid):
+    for product in models.products:
+        if product["id"] == productid:
+            selectedProduct = models.products.index(product)
+            return jsonify({"selected":models.products[selectedProduct]})
+    else:
+        return jsonify({"result":"product not found"})
+    # del models.products[selectedProduct]
+
+@app.route('/updateproduct/<int:productid>',methods=['PUT'])
+def editProduct(productid):
+    for product in models.products:
+        if product["id"] == productid:
+            selectedProduct = models.products.index(product)
+            return jsonify({"selected":models.products[selectedProduct]})
+    else:
+        return jsonify({"result":"product not found"})
+    # print(selectedProduct)
+    # return jsonify({"resultStatus":"successfully done"})
+
+@app.route('/product/<int:productid>',methods=['GET'])
+def singleProduct(productid):
+    for product in models.products:
+        if product["id"] == productid:
+            return jsonify({"product":product})
+    else:
+        return jsonify({"result":"product not found"})
